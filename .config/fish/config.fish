fb68a301f0dda8b333bfddea9d6d387cd00aa1bf
r181104 <sten181104@gmail.com>
2025-08-23 03:23:24 +0530

    s

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -135,6 +135,17 @@ alias gp 'git push'
 alias git-clean 'git reflog expire --expire=now --all; git gc --prune=now --aggressive'
 
 #  =========================================
+#              ARCH ALIASES
+#  =========================================
+alias i='paru --noconfirm -S --needed'
+alias u='paru --noconfirm -Syu'
+alias r='paru -Rns'
+alias s='paru -Ss'
+alias remove-orphaned='sudo pacman -Rns $(pacman -Qtdq) && paru -Rns $(pacman -Qtdq)'
+alias aggressively-clear-cache='sudo pacman -Scc && paru -Scc'
+alias clear-cache='sudo pacman -Sc && paru -Sc'
+
+#  =========================================
 #           UTILITY ALIASES
 #  =========================================
 alias kssh "kitty +kitten ssh"
