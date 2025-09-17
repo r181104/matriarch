6f148d512ab85d25cf27e78bf37808f7bcb50042
r181104 <sten181104@gmail.com>
2025-09-17 20:15:40 +0530

    s

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -343,7 +343,7 @@ end
 # ==============================
 function man --description "Pretty man pages with bat when available"
     if type -q bat
-        command man $argv | col -bx | bat -l man --paging=always --theme=TwoDark
+        command man $argv | col -bx | bat -l man --paging=always
     else
         command man $argv
     end
