714dc61858c0fe889e6dffbb5918039ff61a64f4
r181104 <sten181104@gmail.com>
2025-09-22 16:26:09 +0530

    s

--- .config/nvim/lua/auto.lua
+++ .config/nvim/lua/auto.lua
@@ -4,6 +4,13 @@ vim.api.nvim_create_autocmd("VimEnter", {
 	end,
 })
 
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*",
+	callback = function(args)
+		require("conform").format({ bufnr = args.buf })
+	end,
+})
+
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking (copying) text",
 	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
